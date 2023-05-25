16052fd347bbdf08f3a5fd0f837c0097aeb80052
JLMSC <reecaptcha@gmail.com>
2023-05-25 10:59:06 -0300

    Remoção do campo 'tables' no painel do 'admin', 'tables' é inicializado durante a criação de um 'restaurant'.

--- webapp/restaurant/admin.py
+++ webapp/restaurant/admin.py
@@ -7,3 +7,4 @@ class RestaurantAdmin(admin.ModelAdmin):
     list_display = ('name', 'formatted_cnpj', 'formatted_phone', 'city', 'state')
     list_filter = ('state',)
     search_fields = ('name', 'phone_number', 'cnpj')
+    exclude = ('tables',)
