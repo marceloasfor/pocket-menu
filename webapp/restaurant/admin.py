143c7e92fdfec515ca01c642eb0e55a272a92b54
JLMSC <reecaptcha@gmail.com>
2023-05-27 22:31:46 -0300

    Adicionado suporte para configuração de mesas separadamente.

--- webapp/restaurant/admin.py
+++ webapp/restaurant/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from . import models
+from .models import Restaurant
+from table.admin import TableInline
 
-@admin.register(models.Restaurant)
+@admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
-    list_display = ('name', 'formatted_cnpj', 'formatted_phone', 'city', 'state')
+    list_display = ('name', 'formatted_cnpj', 'formatted_phone', 'city', 'state',)
     list_filter = ('state',)
     search_fields = ('name', 'phone_number', 'cnpj')
-    exclude = ('tables',)
+    inlines = [TableInline]
