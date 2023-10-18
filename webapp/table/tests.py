6e998381fe74fce2229bcbf837def315bf57f7da
Joan Lucas <reecaptcha@gmail.com>
2023-10-18 11:38:44 -0300

    Finalizei os testes unitários com o modelo 'Mesa'.

--- webapp/table/tests.py
+++ webapp/table/tests.py
@@ -1,23 +1,103 @@
+from django.test import Client
 from django.test import TestCase
-from django.urls import reverse
 from django.contrib.auth.models import User
 
+from .models import Table
+from restaurant.models import Restaurant
+
 class TableTestCase(TestCase):
+    """Realiza testes em cima do modelo de Mesas no painel do admin."""
+
     def setUp(self):
+        # Configura um superusuário para realizar os testes.
+        self.superuser_username = 'test_case'
+        self.superuser_email = 'test@case.com'
+        self.superuser_password = 'test_case'
         self.superuser = User.objects.create_superuser(
-            username='table_test_case',
-            email='admin@example.com',
-            password='supersecret'
+            username=self.superuser_username,
+            email=self.superuser_email,
+            password=self.superuser_password,
         )
 
-    def test_table_page(self):
-        """Testa se a disponibilidade da página de mesa em admin."""
-        self.client.login(username='table_test_case', password='supersecret')
-        response = self.client.get('http://localhost:8000/admin/table/table/')
-        self.assertEqual(response.status_code, 200, "Não foi possível acessar a página de admin de mesa.")
 
-    def test_table_add(self):
-        """Testa a criação de mesas."""
-        # FIXME: Por algum motivo eu não to conseguindo fazer um post.....
-        self.assertTrue(True)
+    def test_login_superuser(self):
+        """Testa autenticação como superusuário."""
+        c = Client()
+        logged_in = c.login(username=self.superuser_username, password=self.superuser_password)
+        self.assertTrue(logged_in, 'Não foi possível entrar como super usuário.')
+        c.logout()
  
+
+    def test_admin_restaurant_page_up(self):
+        """Testa se a página de restaurante no painel do admin é acessível."""
+        c = Client()
+        logged_in = c.login(username=self.superuser_username, password=self.superuser_password)
+        self.assertTrue(logged_in, 'Não foi possível entrar como super usuário.')
+
+        response = c.get('/admin/restaurant/restaurant/')
+        self.assertEqual(response.status_code, 200, 'Não foi possível acessar a página de restaurante no painel de admin.')
+        c.logout()
+
+
+    def test_admin_add_new_restaurant(self):
+        """Testa se um superusuário consegue adicionar um novo restaurante."""
+        c = Client()
+        logged_in = c.login(username=self.superuser_username, password=self.superuser_password)
+        self.assertTrue(logged_in, 'Não foi possível entrar como super usuário.')
+
+        response = c.get('/admin/restaurant/restaurant/')
+        self.assertEqual(response.status_code, 200, 'Não foi possível acessar a página de restaurante no painel de admin.')
+
+        data = {'name': 'test_restaurant_add', 'description': 'None', 'cnpj': 11111111111111, 'postal_code': 11111111,
+                'address_line1': 'None', 'city': 'None', 'state': 'ac', 'phone': 11111111111, 'table_set-0-capacity': '1',
+                'table_set-0-available': True, 'table_set-0-number': '1', 'table_set-0-verification_code': '1234',}
+        # Essas duas linhas são obrigatórias pq os campos de 'table' são ocultos qnd vai adicionar um restaurante.
+        data['table_set-TOTAL_FORMS'] = 1
+        data['table_set-INITIAL_FORMS'] = 0
+        response = c.post('/admin/restaurant/restaurant/add/', data)
+        self.assertEqual(response.status_code, 302, 'Não foi possível criar um restaurante no painel do admin (algum campo está errado).')
+        self.assertTrue(Restaurant.objects.filter(name='test_restaurant_add').exists(), 'O restaurante foi criado mas não foi adicionado ao banco de dados.')
+        c.logout()
+
+
+    def test_admin_table_page_up(self):
+        """Testa se a página de mesas no painel do admin é acessível."""
+        c = Client()
+        logged_in = c.login(username=self.superuser_username, password=self.superuser_password)
+        self.assertTrue(logged_in, 'Não foi possível entrar como super usuário.')
+
+        response = c.get('/admin/table/table/')
+        self.assertEqual(response.status_code, 200, 'Não foi possível acessar a página de mesas no painel de admin.')
+        c.logout()
+
+
+    def test_admin_add_new_table(self):
+        """Testa se um superusuário consegue adicionar uma nova mesa."""
+        c = Client()
+        logged_in = c.login(username=self.superuser_username, password=self.superuser_password)
+        self.assertTrue(logged_in, 'Não foi possível entrar como super usuário.')
+
+        response = c.get('/admin/restaurant/restaurant/')
+        self.assertEqual(response.status_code, 200, 'Não foi possível acessar a página de restaurante no painel de admin.')
+
+        data = {'name': 'test_restaurant_add', 'description': 'None', 'cnpj': 11111111111111, 'postal_code': 11111111,
+                'address_line1': 'None', 'city': 'None', 'state': 'ac', 'phone': 11111111111, 'table_set-0-capacity': '1',
+                'table_set-0-available': True, 'table_set-0-number': '1', 'table_set-0-verification_code': '1234',}
+        # Essas duas linhas são obrigatórias pq os campos de 'table' são ocultos qnd vai adicionar um restaurante.
+        data['table_set-TOTAL_FORMS'] = 1
+        data['table_set-INITIAL_FORMS'] = 0
+        response = c.post('/admin/restaurant/restaurant/add/', data)
+        self.assertEqual(response.status_code, 302, 'Não foi possível criar um restaurante no painel do admin (algum campo está errado).')
+        self.assertTrue(Restaurant.objects.filter(name='test_restaurant_add').exists(), 'O restaurante foi criado mas não foi adicionado ao banco de dados.')
+        
+        response = c.get('/admin/table/table/')
+        self.assertEqual(response.status_code, 200, 'Não foi possível acessar a página de mesas no painel de admin.')
+        
+        data = {'restaurant': 1, 'capacity': 1, 'available': True, 'number': 2, 'verification_code': '4321'}
+        # Parece que tem campo oculto quando vai adicionar 'table' tbm...
+        data['Table_users-TOTAL_FORMS'] = 1
+        data['Table_users-INITIAL_FORMS'] = 0
+        response = c.post('/admin/table/table/add/', data)
+        self.assertEqual(response.status_code, 302, 'Não foi possível criar uma mesa no painel do admin (algum campo está errado).')
+        self.assertTrue(Table.objects.filter(number=2).exists(), 'A mesa foi criada mas não foi adicionada ao banco de dados.')
+        c.logout()
