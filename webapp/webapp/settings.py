71e559186b0e080bfe32e2d70e7192bd98630ef2
Marcelo Asfor <asfor@Marcelos-MacBook-Pro.local>
2023-12-04 20:57:08 -0300

    Create index.html

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -10,7 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dfjvp091ofn7y=a@(+-)!cbdzb4h8f%mq8b_h#cv60barv0b*p'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-dfjvp091ofn7y=a@(+-)!cbdzb4h8f%mq8b_h#cv60barv0b*p',
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
